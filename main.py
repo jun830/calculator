9c9db27dee15d493371d278d01d1fe037b6e44b4
Jun830 <90589388+jun830@users.noreply.github.com>
2023-10-01 17:10:35 +0900

    10/01ボタンの配置と基本の演算部の処理

--- main.py
+++ main.py
@@ -1,24 +1,70 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("計算機")
-# root.geometry("400x400")
+def main():
+    root = tk.Tk()
+    root.title("電卓")
+    calc = Calculator(root)
+    root.mainloop()
+
+class Calculator:
+    def __init__(self, root):
+        self.frame = tk.Frame(root)
+        self.frame.grid(row=0, column=0, padx=15, pady=15)
+
+        self.calc_str = ""
+        self.stack = tk.StringVar()
+        self.stack.set("0")
+        self.num_box = tk.Entry(self.frame, font=("MSゴシック", 32),
+                                textvariable=self.stack, justify=tk.RIGHT)
+        self.num_box.grid(row=0, column=0, columnspan=4)
+
+        self.memory = tk.StringVar()
+        self.memory.set("0")
+        # [row, column, number, command]
+        Definition_Button = [["M+","M-","MR","MC"],
+                             ["AC", "C",  "", "/"],
+                             [ "7", "8", "9", "*"],
+                             [ "4", "5", "6", "-"],
+                             [ "1", "2", "3", "+"],
+                             [ "0", ".","DEL","="]]
+
+        for r, d_i in enumerate(Definition_Button, 1):
+            for c, label in enumerate(d_i):
+                button = tk.Button(self.frame, text=label, font=("ＭＳゴシック", 28))
+                button.grid(row=r, column=c, sticky="news")
+                button.bind("<ButtonPress>", self.push_command)
+
+    def push_command(self, e):
+        x = e.widget.cget("text")
+        if x == "M+":
+            pass
+        elif x == "M-":
+            pass
+        elif x == "MR":
+            pass
+        elif x == "MC":
+            self.memory.set("0")
+        elif x == "AC":
+            self.stack.set("0")
+            self.memory.set("0")
+        elif x == "C":
+            self.stack.set("0")
+        elif x == "":pass
+        else:
+            if x == "=":
+                try:
+                    self.calc_str = str(eval(self.calc_str))
+                except:
+                    pass
+            elif x in "+-x/.":
+                self.calc_str += x
+            else:
+                if self.calc_str == "0":
+                    self.calc_str = x
+                else:
+                    self.calc_str += x
+            self.stack.set(self.calc_str)
 
 
-stack = tk.StringVar()
-stack.set("")
-
-ent = tk.Entry(root, textvariable=stack)
-ent.grid(row=0, columnspan=4)
-
-# [row, column, number , command]
-Definition_Button = [[1, 0, "7"], [1, 1, "8"], [1, 2, "9"], [1, 3, "÷"],
-                     [2, 0, "4"], [2, 1, "5"], [2, 2, "6"], [2, 3, "×"],
-                     [3, 0, "1"], [3, 1, "2"], [3, 2, "3"], [3, 3, "-"],
-                     [4, 0, "0"], [4, 1, "C"], [4, 2, "="], [4, 3, "+"]]
-
-for r, c, label in Definition_Button:
-    button = tk.Button(text=label, font=("Times", 20), height=1, width=2)
-    button.grid(row=r, column=c)
-
-root.mainloop()
+if __name__=="__main__":
+    main()
