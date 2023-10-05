f63fa9cdd6a2ab31ac0cbd0e0568ebb1a4634f6b
Jun830 <90589388+jun830@users.noreply.github.com>
2023-10-05 17:30:49 +0900

    10/05完成

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+"""version
+Python 3.9.4
+tkinter 8.6
 """
-設計
+"""設計
 
 表示部分
-    数字を出す
+    num_box 数字を出す
 内部数字回り
     front_num   表出用数字
     back_num    計算用内部数字
     back_ope    計算用内部演算子
     memory_num  メモリー用保存数字
+    last_is_ope 最後に入力されたのが演算子だったかどうか
 ボタン
     数字
         0~9 .(小数点)
-        back_numなしback_opeありのとき back_num = front_num, front_num = 0
+        直前が演算子のとき back_num = front_num, front_num = 0
         else:front_num に入力を追加
     演算子
         +, -, *, /, //, =
-        挙動
-            back_numがあるとき   front_num = back_num back_ope front_num, back_num = back_ope = None
-                無いとき        back_ope = 入力
+        直前の入力が演算子のとき    back_ope を書き換え
+        else:
+            back_numがあるとき   front_num = back_num back_ope front_num
+                            　　back_num = back_ope = None
+                    無いとき     back_ope = 入力
     メモリー機能
         M+ memory_num + front_num
         M- memory_num - front_num
@@ -27,7 +33,6 @@
         AC  front_numとback_numの両方を0
         C   front_numのみを0
         DEL front_numの末尾1桁を消す
-
 """
 import tkinter as tk
 
@@ -75,10 +80,12 @@ class Calculator:
     def push_command(self, e):
         x = e.widget.cget("text")
         if x == "M+":
-            self.memory_num = eval(str(self.memory_num)+ "+" + self.front_num)
+            self.memory_num = eval(str(self.memory_num)+ "+" + self.num_box.get())
+            self.front_num = "0"
 
         elif x == "M-":
-            self.memory_num = eval(str(self.memory_num) + "-" + self.front_num)
+            self.memory_num = eval(str(self.memory_num) + "-" + self.num_box.get())
+            self.front_num = "0"
 
         elif x == "MR":
             self.front_num = str(self.memory_num)
@@ -127,7 +134,7 @@ class Calculator:
                     self.is_decimal = False
                     self.display_num.set(self.back_num)
                 except ZeroDivisionError:
-                    self.display_num = "0で割ってはいけません"
+                    self.display_num.set("0で割ってはいけません")
                     self.memory_num = 0
                     self.front_num = "0"
                     self.back_num = None
@@ -161,8 +168,7 @@ class Calculator:
         else:
             pass
 
-        # print(self.front_num, self.back_num, self.back_ope, self.is_decimal,self.last_is_ope)
-
+        # print(x, self.front_num, self.back_num, self.back_ope, self.is_decimal,self.last_is_ope, self.memory_num)
 
 if __name__=="__main__":
     main()
